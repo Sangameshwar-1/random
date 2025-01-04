b711c2fec32627ac82c3ab38d6a809f06039fb58
SALE SANGAMESHWAR <salesangameshwar1@gmail.com>
2025-01-05 00:43:50 +0530

    Update chat-script.js

--- chat-script.js
+++ chat-script.js
@@ -1,6 +1,5 @@
 // <script> 
 // Firebase configuration
-require('dotenv').config();
 const firebaseConfig = {
     apiKey: process.env.FIREBASE_API_KEY,
     authDomain: process.env.FIREBASE_AUTH_DOMAIN,
