18296c5520001899ee12a77b09fb79d84d33a125
SALE SANGAMESHWAR <salesangameshwar1@gmail.com>
2025-01-05 15:40:21 +0530

    Update chat-script.js

--- chat-script.js
+++ chat-script.js
@@ -1,14 +1,14 @@
 // <script> 
 // Firebase configuration
 const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.FIREBASE_DATABASE_URL,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID,
-    measurementId: process.env.FIREBASE_MEASUREMENT_ID
+    apiKey: "AIzaSyBQOhH2t5XPieLGlM3anZYco06-PvQt37c",
+    authDomain: "adpsr-75e1f.firebaseapp.com",
+    databaseURL: "https://adpsr-75e1f-default-rtdb.firebaseio.com",
+    projectId: "adpsr-75e1f",
+    storageBucket: "adpsr-75e1f.appspot.com",
+    messagingSenderId: "959425930437",
+    appId: "1:959425930437:web:cd3bae057e5b657a32fcac",
+    measurementId: "G-1X15K2TPH4"
 };
 
 // Initialize Firebase
